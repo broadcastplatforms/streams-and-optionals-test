fd0cfc25c6bdff4b95d1110fe25ea64a0ed31713
Christian.Boult <christian.boult@global.com>
2022-04-05 15:17:50 +0100

    feat(): support Java 8

--- src/Main.java
+++ src/Main.java
@@ -14,26 +14,26 @@ public class Main {
 
     // sort the list of ints using the Stream API and print the result
     static void sortList() {
-        List<Integer> ints = List.of(10, 32, 56, 7, 11, 88, 15, 76, 43, 30);
+        List<Integer> ints = Arrays.asList(10, 32, 56, 7, 11, 88, 15, 76, 43, 30);
         System.out.println("Sorted ints: ");
     }
 
     // find all the duplicate elements in the list using the Stream API and print them
     static void findDuplicates() {
-        List<Integer> ints = List.of(10, 32, 10, 7, 11, 88, 15, 11, 43, 32);
+        List<Integer> ints = Arrays.asList(10, 32, 10, 7, 11, 88, 15, 11, 43, 32);
         System.out.println("Duplicates: ");
     }
 
     // print how many instances of each word are in the list e.g. "{red=1, "green=2, "blue=3}"
     static void countInstances() {
-        List<String> colours = List.of("blue", "red", "green", "blue", "green", "blue");
+        List<String> colours = Arrays.asList("blue", "red", "green", "blue", "green", "blue");
         System.out.println("Colour count: ");
     }
 
     // print the first odd number in the list and if there isn't one return -1
     static void findFirstOdd() {
-        List<Integer> ints = List.of(10, 32, 56, 7, 11, 88, 15, 76, 43, 30);
-        List<Integer> intsNoOddNumbers = List.of(10, 32, 56, 10, 18, 88, 16, 76, 42, 30);
+        List<Integer> ints = Arrays.asList(10, 32, 56, 7, 11, 88, 15, 76, 43, 30);
+        List<Integer> intsNoOddNumbers = Arrays.asList(10, 32, 56, 10, 18, 88, 16, 76, 42, 30);
 
         System.out.println("First odd number: ");
         System.out.println("First odd number: ");
